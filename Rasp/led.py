c4ec0339224667ec8d90bcf443fc88934af4db4f
annkwon1123 <annkwon1123@gmail.com>
2023-04-21 21:34:33 +0900

    submit#4

--- Rasp/led.py
+++ Rasp/led.py
@@ -4,14 +4,15 @@ import time
 rg.setmode(rg.BCM)
 
 ledPlusPin = 17
-
-rg.setup(ledPlusPin, rg.OUT)
+greenPin = 27
 
 try:
     while True:
         rg.output(ledPlusPin, rg.LOW)
+        rg.output(greenPin, rg.HIGH)
         time.sleep(1)
         rg.output(ledPlusPin, rg.HIGH)
+        rg.output(greenPin, rg.LOW)
         time.sleep(1)
 except KeyboardInterrupt:
     rg.cleanup()
