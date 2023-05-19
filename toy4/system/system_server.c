bdab84badd433679c0eafe586d02778731d66822
annkwon1123 <annkwon1123@gmail.com>
2023-05-19 16:09:55 +0900

    toy4

--- toy4/system/system_server.c
+++ toy4/system/system_server.c
@@ -25,6 +25,17 @@ int create_system_server()
     printf("여기서 시스템 프로세스를 생성합니다.\n");
 
     /* fork 를 이용하세요 */
-
+    switch (SystemPid = fork()) {
+        case -1:
+                printf("fork failed\n");
+        case 0:
+                /* change process name */
+                if (prctl(PR_SET_NAME, (unsigned ling) name) < 0)
+                        perror("prtcl()");
+                system_server();
+                break;
+        default:
+                break;
+        }    
     return 0;
 }
